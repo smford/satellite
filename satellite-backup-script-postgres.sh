bccb524c92166df9d194194c319e67bf2d2932ba
smford <s.m.ford@gmail.com>
2015-04-03 12:52:35 +0100

    Comments and changes to make script easier to understand

--- satellite-backup-script-postgres.sh
+++ satellite-backup-script-postgres.sh
@@ -2,10 +2,15 @@
 #
 # A simple script for satellite v5.6 to backup and rotate backups
 #
-# 0 5 * * 0 /root/scripts/backup-db-postgres-log-v1.sh >/dev/null 2>&1
+# 0 5 * * 0 /root/scripts/satellite-backup-script-postgres.sh >/dev/null 2>&1
+# The cronjob should be set to run daily, the NUMBACKUPS variable defines how many daily backups to maintain.  Only a small number is needed as ideally 
+# they should be archived within the systems normal regular backup strategy.
 #
 # https://access.redhat.com/documentation/en-US/Red_Hat_Satellite/5.6/html/User_Guide/sect-Red_Hat_Satellite-User_Guide-Backing_up_an_Embedded_Database.html
+
+# uncomment the below line and run the script from the console to see what is happening
 #set -x
+
 BACKUPLOCATION=/var/satellite/backupdb
 BACKUPDATE=$(date "+%F")
 NUMBACKUPS=2
@@ -27,9 +32,7 @@ echo "Starting backup: $(date)"
 echo "Creating backup: ${BACKUPLOCATION}/${BACKUPDATE}/db-backup-${BACKUPDATE}"
 
 db-control online-backup ${BACKUPLOCATION}/${BACKUPDATE}/db-backup-${BACKUPDATE}
-#echo "Dryrun" > ${BACKUPLOCATION}/${BACKUPDATE}/db-backup-${BACKUPDATE}
 
-#CLEANOLD=$(date --date='3 days ago' +%F)
 CLEANOLD=$(date --date="${NUMBACKUPS} days ago" +%F)
 
 
